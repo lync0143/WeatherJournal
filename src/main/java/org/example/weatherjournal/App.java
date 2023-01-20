90bbab69cd880f0971cd0382e4b1fff52078ba4b
Tom Lynch <76807899+lync0143@users.noreply.github.com>
2023-01-19 18:56:24 -0600

    Make the App class a Configuration bean.

--- src/main/java/org/example/weatherjournal/App.java
+++ src/main/java/org/example/weatherjournal/App.java
@@ -2,8 +2,10 @@ package org.example.weatherjournal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import ...
 
 @SpringBootApplication
+@Configuration
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
