eb15d5e712bac3b30f7ac881ed040305f70570e6
Tom Lynch <lync0143@gmail.com>
2023-01-19 19:11:11 -0600

    Fix the import of the Configuration annotation.

--- src/main/java/org/example/weatherjournal/App.java
+++ src/main/java/org/example/weatherjournal/App.java
@@ -2,7 +2,8 @@ package org.example.weatherjournal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-// import ...
+import org.springframework.context.annotation.Configuration;
+
 
 @SpringBootApplication
 @Configuration
